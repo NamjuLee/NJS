ad5634020f6b344deff1770df427098ddf407fa7
NJ <nj.namju@gmail.com>
2016-08-26 14:45:40 -0700

    update

--- GLCanvas.js
+++ GLCanvas.js
@@ -187,23 +187,15 @@ GLWindow.prototype.getIntersectionAtScreenCoordinates=function(x,y) {
     var nx = ( x / self.width ) * 2 - 1;
     var ny = - ( y / self.height ) * 2 + 1;
     var vector = new THREE.Vector3( nx, ny, 0.5 );
-
-    // self.projector.unprojectVector( vector, self.camera );
     self.raycaster = new THREE.Raycaster( self.camera.position, vector.sub( self.camera.position ).normalize() );
-
-
     var intersects = self.raycaster.intersectObjects( self.objects );
-
     if ( intersects.length > 0 ) {
         return intersects[ 0 ];
     }
-
     return null;
 }
 GLWindow.prototype.getPointAtScreenCoordinates=function(x,y) {
-    
     var intersects = getIntersectionAtScreenCoordinates(x, y);
-
     if ( intersects ) {
         return intersects.point;
     }
@@ -226,13 +218,6 @@ GLWindow.prototype.Remove=function(obj){
 }
 GLWindow.prototype.RemoveAll=function(){
     var self = this;
-    for (var i = self.scene.children.length - 1; i >= 0; i--) {
-        // self.scene.children = []
-        // self.scene.remove(self.scene.children[i])
-        // self.Remove(self.objects[i]);
-        // self.scene.remove(self.objects[i]);
-    }
-
     for (var i = self.objects.length - 1; i >= 0; i--) {
         self.scene.remove(self.objects[i])
     }
@@ -250,19 +235,9 @@ GLWindow.prototype.render=function() {
     self.Update();
     self.renderer.render(self.scene, self.camera);
 }
-
-
 //................................................................................. public 
 GLWindow.prototype.Init=function() {
     console.log("this is Init")
-
-    var self = this;
-    var cubegeometry = new THREE.BoxGeometry(0.2,0.2,0.2);
-    var cubematerial = new THREE.MeshPhongMaterial( { ambient: 0x030303, color: 0xdddddd, specular: 0xffffff, shininess: 30, shading: THREE.FlatShading } );
-    var newcube = new THREE.Mesh( cubegeometry, cubematerial );
-    newcube.castShadow = true;
-    newcube.receiveShadow = true;
-    self.Add(newcube);
 
     
 };
@@ -271,21 +246,3 @@ GLWindow.prototype.Update=function() {
 
 
 };
-function updateGround() {
-    t+=0.1;
-    var x,z;
-     for ( var i = 0, l = groundGeometry.vertices.length; i < l; i ++ ) {
-        x=groundGeometry.vertices[ i ].x;
-        z=groundGeometry.vertices[ i ].z;
-        var dx=x-mouse3d.x;
-        var dz=z-mouse3d.z;
-        var d=Math.sqrt(dx*dx+dz*dz);
-        var mu=Math.exp(-0.3*d*d)*2.0;
-        groundGeometry.vertices[ i ].y = Math.random() * 0.01+Math.cos(t+x*3.0)*0.2-0.4-mu;
-
-    }
-    groundGeometry.computeFaceNormals();
-    groundGeometry.computeVertexNormals();
-    groundGeometry.verticesNeedUpdate = true;
-    groundGeometry.normalsNeedUpdate = true;
-}
